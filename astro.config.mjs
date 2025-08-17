a7604143013837a060abfa47a16c577779566895
Mauricio Garcia <mauriciogaraco@gmail.com>
2025-08-17 13:38:49 -0500

    chanfe astro.config.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://mauriciogaraco.github.io',
   base: '/Portfolio',
+  integrations: [react()],
   trailingSlash: 'ignore', // opcional
 });
