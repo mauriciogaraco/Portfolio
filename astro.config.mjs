79b57e3d867223effcf239842af6636cf66a1efc
Mauricio Garcia <mauriciogaraco@gmail.com>
2025-08-17 13:30:09 -0500

    add 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
-  // Agrega esta línea
-  base: '/Portfolio/', 
-  integrations: [react()]
+  site: 'https://mauriciogaraco.github.io',
+  base: '/Portfolio',
+  trailingSlash: 'ignore', // opcional
 });
